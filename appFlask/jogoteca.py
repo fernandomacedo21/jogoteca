137e3f05382b82ccc5536323b9b267107cf7c25b
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-03-17 16:27:26 -0300

    Criação de novo jogo; Criando itens para aplicação; Como montar formularios usando Flask; Resolver POSTs no servidor

--- appFlask/jogoteca.py
+++ appFlask/jogoteca.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template
+import lista as lista
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Mortal Kombat', 'LUTA', 'SNES')
-    jogo4 = Jogo('Street Fighter', 'LUTA', 'SNES')
-    lista = [jogo1, jogo2, jogo3, jogo4]
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'LUTA', 'SNES')
+jogo4 = Jogo('Street Fighter', 'LUTA', 'SNES')
+lista = [jogo1, jogo2, jogo3, jogo4]
+
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run()
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogo', jogos=lista)
+
+
+app.run(debug=True)
